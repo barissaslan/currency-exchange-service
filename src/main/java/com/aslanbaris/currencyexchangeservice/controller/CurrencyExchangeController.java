0318608febf046be0fcd615af549f8d0a36507a3
Barış Aslan <aslannbaris@gmail.com>
2020-12-16 00:18:37 +0300

    update

--- src/main/java/com/aslanbaris/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/aslanbaris/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,12 +2,14 @@ package com.aslanbaris.currencyexchangeservice.controller;
 
 import com.aslanbaris.currencyexchangeservice.bean.ExchangeValue;
 import com.aslanbaris.currencyexchangeservice.repository.ExchangeValueRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class CurrencyExchangeController {
 
@@ -21,6 +23,9 @@ public class CurrencyExchangeController {
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("server.port")));
+
+        log.info("exchange: {}", exchangeValue);
+
         return exchangeValue;
     }
 
